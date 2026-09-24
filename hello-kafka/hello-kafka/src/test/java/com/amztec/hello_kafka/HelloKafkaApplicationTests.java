7af533928ed5291e5712b913cd2ba456673e9a26
Engenheiro Muniz <engenheiro.muniz@outlook.com>
2026-09-23 23:36:23 -0300

    chore(hello-kafka): configura conexão com Kafka e serializadores String
    Define bootstrap em localhost:9092, consumer group hello-group com
    auto-offset-reset earliest e serialização String para chave e valor.

---
+++ hello-kafka/hello-kafka/src/test/java/com/amztec/hello_kafka/HelloKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amztec.hello_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
